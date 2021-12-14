456b4f4485237287af3d28bb6f77a5084dd019ea
Helen <elena.kozovyakina@gmail.com>
2021-12-14 21:36:50 +0300

    mode

--- oop.py
+++ oop.py
@@ -98,24 +98,244 @@
 # print(h1.get_birthday())
 # h1.print_info()
 
-class Person:
-    skill = 10  # квалификация сотрудника
+# class Person:
+#     skill = 10  # квалификация сотрудника
+#
+#     def print_info(self, name, surname):
+#         self.name = name
+#         self.surname = surname
+#         res = f"Данные сотрудника: {self.name} {self.surname}"
+#         print(res)
+#
+#     def add_skill(self, k):
+#         self.skill += k
+#         print(f"Квалификация сотрудника {self.name}: {self.skill}")
+#
+#
+# p1 = Person()
+# p1.print_info("Viktor", "Reznik")
+# p1.add_skill(3)
+# print()
+# p2 = Person()
+# p2.print_info("Anna", "Dolgih")
+# p2.add_skill(2)
 
-    def print_info(self, name, surname):
-        self.name = name
-        self.surname = surname
-        res = f"Данные сотрудника: {self.name} {self.surname}"
-        print(res)
-
-    def add_skill(self, k):
-        self.skill += k
-        print(f"Квалификация сотрудника {self.name}: {self.skill}")
+# def sum_arg(a, b):
+#     print(a + b)
+#
+#
+# sum_arg(5, 2)
+# sum_arg("Hello", "world")
 
 
-p1 = Person()
-p1.print_info("Viktor", "Reznik")
-p1.add_skill(3)
-print()
-p2 = Person()
-p2.print_info("Anna", "Dolgih")
-p2.add_skill(2)
+# __магическийМетод__
+
+# Специальные методы
+# конструктор  __new__
+# инициализатор  __init__
+# деструктор  __del__
+
+
+# class Point:
+#     count = 0
+#
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#         Point.count += 1
+#
+#     def check_val(z):
+#         if isinstance(z, int) or isinstance(z, float):
+#             return True
+#         return False
+#
+#     def set_coords(self, x, y):
+#         if Point.check_val(x) and Point.check_val(y):
+#             self.x = x
+#             self.y = y
+#         else:
+#             print("Координаты должны быть числами")
+#
+#     def get_coords(self):
+#         return self.x, self.y
+#
+#
+# p1 = Point(5, 10)
+# # p1.set_coords(2.3, 5.6)
+# print(p1.get_coords())
+# print(p1.__dict__)
+
+# class Robot:
+#     k = 0
+#
+#     def __init__(self, name):
+#         self.name = name
+#         print("Инициализация робота:", self.name)
+#         Robot.k += 1
+#
+#     def __del__(self):
+#         print(self.name, "выключается!")
+#         Robot.k -= 1
+#
+#         if Robot.k == 0:
+#             print(self.name, "был последним")
+#         else:
+#             print("Работающих роботов осталось:", Robot.k)
+#
+#     def say_hi(self):
+#         print("Приветствую! Меня зовут:", self.name)
+#
+#
+# droid1 = Robot("R2-D2")
+# droid1.say_hi()
+# print("Численность роботов:", Robot.k)
+# droid2 = Robot("C-3PO")
+# droid2.say_hi()
+# print("Численность роботов:", Robot.k)
+# droid3 = Robot("PO")
+# droid4 = Robot("P4")
+#
+# print("\nЗдесь роботы могут проделать какую-то работу\n")
+# print("Роботы закончили свою работу. Давайте их выключим.")
+# del droid1
+# del droid2
+# del droid3
+# del droid4
+
+# print("Численность роботов:", Robot.k)
+
+class Point:
+    WIDTH = 5
+    z = 100
+
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+
+
+    # def __getattr__(self, name):
+    #     return f"В классе {__class__.__name__} отсутствует атрибут: {name}"
+
+    # def __getattribute__(self, item):
+    #     if item == "_Point__x":
+    #         return 0
+    #     else:
+    #         return object.__getattribute__(self, item)
+
+    def __setattr__(self, key, value):
+        if key == "z":
+            raise AttributeError
+        else:
+            self.__dict__[key] = value
+
+    def check_val(z):
+        if isinstance(z, int) or isinstance(z, float):
+            return True
+        return False
+
+    def set_coords(self, x, y):
+        if Point.check_val(x) and Point.check_val(y):
+            self.__x = x
+            self.__y = y
+        else:
+            print("Координаты должны быть числами")
+
+    def set_coords_x(self, x):
+        if Point.check_val(x):
+            self.__x = x
+        else:
+            print("Координаты должны быть числами")
+
+    def set_coords_y(self, y):
+        if Point.check_val(y):
+            self.__y = y
+        else:
+            print("Координаты должны быть числами")
+
+    def get_coords(self):
+        return self.__x, self.__y
+
+    def get_coords_x(self):
+        return self.__x
+
+    def get_coords_y(self):
+        return self.__y
+
+    def area(self):
+        return (self.__x * self.__y) + self.z
+
+
+p1 = Point(5, 10)
+# print(p1.__x)
+# print(p1._Point__x)
+# print(p1.zzz)
+# print(p1.get_coords())
+# p1.set_coords(2, 3)
+# print(p1.get_coords())
+# p1.z = 200
+# Point.WIDTH = 10
+print(p1.area())
+# p1.set_coords_x(100)
+# p1.set_coords_y(50)
+# print(p1.get_coords())
+# print(p1.get_coords_x())
+# print(p1.get_coords_y())
+# print(p1.__dict__)
+# print(p1._Point__x)
+# p1._Point__x = 111
+# print(p1.__dict__)
+# print(p1.__x, p1.__y)
+# p1.__x = 100
+# p1.__y = "abc"
+# print(p1.__x, p1.__y)
+
+# Инкапсуляция
+# x - public
+# _x - protected
+# __x - private
+
+# import math
+#
+#
+# class Rectangle:
+#     def __init__(self, length=1, width=1):
+#         self.__length = length
+#         self.__width = width
+#
+#     def set_length(self, length):
+#         self.__length = length
+#
+#     def set_width(self, width):
+#         self.__width = width
+#
+#     def get_length(self):
+#         return self.__length
+#
+#     def get_width(self):
+#         return self.__width
+#
+#     def square(self):
+#         return self.__length * self.__width
+#
+#     def perimetr(self):
+#         return (self.__length * self.__width) * 2
+#
+#     def hypo(self):
+#         return math.sqrt(self.__length ** 2 + self.__width ** 2)
+#
+#     def get_draw(self):
+#         # for i in range(self.__length):
+#         #     print(self.__width * "*")
+#         print((self.__width * "*" + "\n") * self.__length)
+#
+#
+# rec1 = Rectangle()
+# rec1.set_length(3)
+# rec1.set_width(9)
+# print('Длина прямоугольника', rec1.get_length())
+# print('Ширина прямоугольника', rec1.get_width())
+# print('Площадь', rec1.square())
+# print('Периметр', rec1.perimetr())
+# print('Гипотенуза', round(rec1.hypo(), 2))
+# rec1.get_draw()
