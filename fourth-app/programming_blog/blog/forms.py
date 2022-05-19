8fd2761eac6a09520d4aed0cf1d1bba9675e07a0
Helen <elena.kozovyakina@gmail.com>
2022-05-19 21:34:00 +0300

    blog_admin

--- fourth-app/programming_blog/blog/forms.py
+++ fourth-app/programming_blog/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from captcha.fields import CaptchaField
 from .models import *
 
 
@@ -37,3 +37,25 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Имя', max_length=255)
+    email = forms.EmailField(label='Email')
+    content = forms.CharField(label='Сообщение', widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
+    captcha = CaptchaField()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
